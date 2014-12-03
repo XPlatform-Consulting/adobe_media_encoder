c60ac38efc14ced89532144b1cf9e9de1c3f9a1b
John Whitson <john.whitson@gmail.com>
2014-12-03 11:39:54 -0700

    Fixed argument handling issues

--- lib/adobe_media_encoder/api/cli.rb
+++ lib/adobe_media_encoder/api/cli.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'optparse'
 require 'pp'
 
 require 'adobe_media_encoder/api/client'
@@ -10,11 +11,11 @@ module AdobeMediaEncoder
     class CLI
 
       LOGGING_LEVELS = {
-          :debug => Logger::DEBUG,
-          :info => Logger::INFO,
-          :warn => Logger::WARN,
-          :error => Logger::ERROR,
-          :fatal => Logger::FATAL
+        :debug => Logger::DEBUG,
+        :info => Logger::INFO,
+        :warn => Logger::WARN,
+        :error => Logger::ERROR,
+        :fatal => Logger::FATAL
       }
 
       attr_accessor :logger, :api
@@ -23,7 +24,7 @@ module AdobeMediaEncoder
         arguments = {
           :log_to => STDERR,
           :log_level => Logger::WARN,
-          :arguments_file_path => File.expand_path(File.basename($0, '.*'), '~/.options'),
+          :options_file_path => File.expand_path(File.basename($0, '.*'), '~/.options'),
         }
         op = OptionParser.new
         op.on('--host-address HOSTADDRESS', 'The AdobeAnywhere server address.',
@@ -36,11 +37,11 @@ module AdobeMediaEncoder
         op.on('--log-to FILENAME', 'Log file location.', "\tdefault: STDERR") { |v| arguments[:log_to] = v }
         op.on('--log-level LEVEL', LOGGING_LEVELS.keys, "Logging level. Available Options: #{LOGGING_LEVELS.keys.join(', ')}",
               "\tdefault: #{LOGGING_LEVELS.invert[arguments[:log_level]]}") { |v| arguments[:log_level] = LOGGING_LEVELS[v] }
-        op.on('--[no-]arguments-file [FILENAME]', 'Path to a file which contains default command line arguments.', "\tdefault: #{arguments[:arguments_file_path]}" ) { |v| arguments[:arguments_file_path] = v}
+        op.on('--[no-]options-file [FILENAME]', 'Path to a file which contains default command line arguments.', "\tdefault: #{arguments[:options_file_path]}" ) { |v| arguments[:options_file_path] = v}
         op.on_tail('-h', '--help', 'Show this message.') { puts op; exit }
         op.parse!(ARGV.dup)
 
-        arguments_file_path = arguments[:arguments_file_path]
+        arguments_file_path = arguments[:options_file_path]
         # Make sure that arguments from the command line override those from the arguments file
         op.parse!(ARGV.dup) if op.load(arguments_file_path)
         arguments
@@ -53,14 +54,10 @@ module AdobeMediaEncoder
         logger.level = args[:log_level] if args[:log_level]
         args[:logger] = logger
 
-        #puts args.inspect; exit
-        #puts args[:method_arguments].inspect#; exit
-        #puts args[:method_arguments]; exit
-
         @api = AdobeMediaEncoder::API::Client.new(args)
 
         ## LIST METHODS
-        #methods = api.methods; methods -= Object.methods; methods.sort.each { |method| puts "#{method} #{api.method(method).parameters rescue ''}" }; exit
+        methods = api.methods; methods -= Object.methods; methods.sort.each { |method| puts "#{method} #{api.method(method).parameters rescue ''}" }; exit
 
         # http.log_request_body = true
         # http.log_response_body = true
@@ -87,10 +84,11 @@ module AdobeMediaEncoder
         puts response
 
         exit
-      end # send
+      end
 
-    end # CLI
-
-  end # API
-
-end # AdobeAnywhere
+      # CLI
+    end
+    # API
+  end
+  # AdobeMediaEncoder
+end
